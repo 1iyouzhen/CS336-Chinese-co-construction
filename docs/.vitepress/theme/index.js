0c6d37ace6bd77d6ec843720b7260f9163a7551e
FuTseYi <futseyi@gmail.com>
2026-05-22 12:50:12 +0800

    feat: enable image zoom

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,4 +1,28 @@
 import DefaultTheme from 'vitepress/theme'
+import { nextTick, onMounted, watch } from 'vue'
+import { useRoute } from 'vitepress'
+import mediumZoom from 'medium-zoom'
 import './custom.css'
 
-export default DefaultTheme
+export default {
+  extends: DefaultTheme,
+  setup() {
+    const route = useRoute()
+
+    const initZoom = () => {
+      mediumZoom('.vp-doc img:not(a img)', {
+        background: 'var(--vp-c-bg)',
+        margin: 24
+      })
+    }
+
+    onMounted(() => {
+      initZoom()
+    })
+
+    watch(
+      () => route.path,
+      () => nextTick(initZoom)
+    )
+  }
+}
